1ca8fe3369859c120c4cf5972e216e5834ca15eb
Navyasinghal1234 <write24navya@gmail.com>
2025-11-09 21:46:53 +0530

    q2.c

---
+++ q2.c
@@ -0,0 +1,12 @@
+int main() {
+    int a,b;
+    int sum,diff,product,quotient;
+    printf("enter a numbers");
+    scanf("%d %d", &a,&b);
+    sum=a+b;
+    diff=a-b;
+    product=a*b;
+    quotient=a/b;
+    printf("sum =%d,diff =%d,product =%d,quotient =%d\n",sum,diff,product,quotient);
+    return 0;
+}
