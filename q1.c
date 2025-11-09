d17755b2df6f6e3588ce0d6840e53b2479f9be8a
Navyasinghal1234 <write24navya@gmail.com>
2025-11-09 21:44:44 +0530

    q1.c

---
+++ q1.c
@@ -0,0 +1,13 @@
+//WAP to input two numbers and display their sum.
+#include <stdio.h>
+
+int main() {
+    int a,b,sum=0;
+    
+    printf("enter a numbers\n");
+    scanf("%d %d", &a,&b);
+    sum=a+b;
+    printf("enter= %d\n",sum);
+
+    return 0;
+}
