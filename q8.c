a8f051bcb2be976bc87523c6c2a316a0e95dbfb4
Navyasinghal1234 <write24navya@gmail.com>
2025-11-09 21:58:47 +0530

    q8.c

---
+++ q8.c
@@ -0,0 +1,14 @@
+//WAP to find sum of first n natural numbers.
+#include <stdio.h>
+
+int main() {
+    int n,i,sum=0;
+    printf("enter the value of n: ");
+    scanf("%d", &n);
+    for(i = 1; i <=n; i++) {
+    sum = sum+i;
+    }
+    printf("the sum of first %d natural numbers is: %d\n", n,sum);
+
+    return 0;
+}
